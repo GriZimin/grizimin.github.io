22c5d9f6d016cfd6e0fd8624bafd33f0558bd17d
GriZimin <grizimin@gmail.com>
2022-12-01 18:42:36 +0500

    asp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GriZimin.github.io.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
